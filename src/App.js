29a638f0ba10e21ceabcc6493d2b8c15eb3b168f
Michael Lennheden <michael@lennheden.com>
2022-03-31 00:31:29 +0200

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import DogImage from './components/dogs/dogs';
 import {useEffect, useState} from "react";
 import './App.css';
 
-
 function App() {
 
   const [dogUrlArray, setDogUrlArray] = useState([]);
   const [dogUrl, setDogUrl] = useState('');
 
-  useEffect(() => {
-    const temp = [];
-    for (let i = 0; i < localStorage.length; i++) {
-      temp.push(localStorage.getItem(localStorage.key(i)));
-    }
-    setDogUrlArray(temp);
+  const handleClick = async () => {
 
-    handleClick();
-  }, []);
+    const response = await fetch('https://random.dog/woof.json');
+    const body = await response.json();
+
+    setDogUrl(body.url)
+  };
 
   useEffect(() => {
 
@@ -35,25 +32,24 @@ function App() {
     setDogUrlArray([...dogUrlArray, JSON.stringify(urlObj)])
   }, [dogUrl]);
 
+  useEffect(() => {
+    const temp = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      temp.push(localStorage.getItem(localStorage.key(i)));
+    }
+    setDogUrlArray(temp);
 
-  const handleClick = async () => {
-
-    const response = await fetch('https://random.dog/woof.json');
-    const body = await response.json();
-
-    setDogUrl(body.url)
-
-  };
-
+    handleClick();
+  }, []);
+  
   return (
     <div className="App">
-      <DogImage src={dogUrl} width="500" height="500"/>
+      <DogImage src={dogUrl}/>
       <button className="Button1" onClick={() => handleClick()}>Show a new dog!</button>
       <ul>
         {
           dogUrlArray.map((imageUrl, index) => {
             const imageObj = JSON.parse(imageUrl);
-
             return (
               <li key={index} onClick={() => {
                 localStorage.removeItem(imageObj.key);
@@ -61,7 +57,6 @@ function App() {
                   ...dogUrlArray.slice(0, index),
                   ...dogUrlArray.slice(index + 1, dogUrlArray.length)
                 ]);
-
               }}>{imageObj.url}</li>
             )
           })
@@ -69,9 +64,6 @@ function App() {
       </ul>
     </div>
   );
-
-    
-
 }
 
 export default App;
